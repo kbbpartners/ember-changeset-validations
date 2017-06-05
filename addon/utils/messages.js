0477e8e65ffa50e733ef1ca6effbe686dce79661
Sol Irvine <sol@turnstilelive.com>
2017-06-05 09:58:45 +0900

    Fixed backward presence and blank messages

--- addon/utils/messages.js
+++ addon/utils/messages.js
@@ -9,6 +9,9 @@ const assign = Ember.assign || Ember.merge;
 const Messages = assign({}, _Messages);
 
 export default assign(Messages, {
+  blank: '{description} must be blank',
+  present: '{description} can\'t be blank',
+
   getDescriptionFor(key = '') {
     return capitalize(dasherize(key).split(/[_-]/g).join(' '));
   }
